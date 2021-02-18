b809e21f995ae6025a64b54e7a7b942573419426
Eslam Muhamed Taha Ibrahim <45505496+Eslam856@users.noreply.github.com>
2021-02-18 07:02:35 +0200

    First Commit

---
+++ BooksAPI/BooksAPI/Models/BooksDBContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BooksAPI.Models
+{
+    public partial class BooksDBContext : DbContext
+    {
+        public BooksDBContext()
+        {
+        }
+
+        public BooksDBContext(DbContextOptions<BooksDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.ToTable("Book");
+
+                entity.Property(e => e.Author).HasMaxLength(50);
+
+                entity.Property(e => e.PublishedAt).HasColumnType("datetime");
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
